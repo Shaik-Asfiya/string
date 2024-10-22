6124e887898fd7bbc808c6714828c8f5da83e5ce
Shaik-Asfiya <154882635+Shaik-Asfiya@users.noreply.github.com>
2024-10-22 11:08:50 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,16 @@
+...
+ Write a  Python Program to calculate the number of words and characters present in a string.
+...
+test_string = "Asfiya"
+ 
+ 
+# printing original string
+print("The original string is : " + test_string)
+ 
+# using split()
+# to count words in string
+res = len(test_string.split())
+ 
+# printing result
+print("The number of words in string are : " + str(res))
+print("The number of words in string are : ", len(test_string))
